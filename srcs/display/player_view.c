29816043b9b4e06500638e2551c90e19bbdf4296
Chi-khang DUONG <chduong@e1r12p17.42.fr>
2022-06-07 15:18:32 +0200

    rien

--- srcs/display/player_view.c
+++ srcs/display/player_view.c
@@ -6,7 +6,7 @@
 /*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 13:35:31 by chduong           #+#    #+#             */
-/*   Updated: 2022/06/06 21:15:48 by chduong          ###   ########.fr       */
+/*   Updated: 2022/06/07 14:50:43 by chduong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,25 @@ static void	draw_background(t_cube *s)
 	}
 }
 
-void	draw_wall(int x, t_cub3d *cub, t_ray ray, t_data *text)
+void	draw_stripe(int x, t_cube *s, t_ray ray, t_data *text)
 {
 	int		y_off;
 	int		x_off;
 	int		y_start;
 	int		y_end;
 
-	y_start = (cub->stripeh[x] > cub->w_height) ?
-	0 : (cub->w_height / 2 - cub->stripeh[x] / 2);
-	y_end = (cub->stripeh[x] > cub->w_height) ?
-	cub->w_height : y_start + cub->stripeh[x];
+	if (cub->stripeh[x] <= cub->w_height)
+		y_start = (cub->w_height / 2 - cub->stripeh[x] / 2);
+	if (cub->stripeh[x] > cub->w_height)
+		y_end =	cub->w_height
+	else
+		y_end = y_start + cub->stripeh[x];
 	if (ray.hor)
 		x_off = (int)ray.inter.x % TILE_SIZE;
 	else
 		x_off = (int)ray.inter.y % TILE_SIZE;
 	x_off = (x_off * text->width) / TILE_SIZE;
+	
 	y_off = (cub->stripeh[x] > cub->w_height) ?
 	(cub->stripeh[x] / 2 - cub->w_height / 2) : 0;
 	while (y_start < y_end)
@@ -60,7 +63,7 @@ void	draw_wall(int x, t_cub3d *cub, t_ray ray, t_data *text)
 	}
 }
 
-void	render_wall(t_cub3d *cub, char *img_data)
+void	draw_wall(t_cube *s, char *img_data)
 {
 	t_ray	ray;
 	int		pix;
@@ -71,13 +74,12 @@ void	render_wall(t_cub3d *cub, char *img_data)
 	draw_background(cub);
 	while (pix < cub->w_width)
 	{
-		angle = normalize_angle(cub->player.angle +
-		atan((pix - (cub->w_width / 2)) / cub->dstprojplane));
+		angle = normalize_angle(s->pov + atan((pix - (cub->w_width / 2)) / cub->dstprojplane));
 		ray = raycast(cub, angle);
 		cub->raydst[pix] = ray.dst;
 		ray.dst *= cos(angle - cub->player.angle);
 		cub->stripeh[pix] = floor((TILE_SIZE / ray.dst) * cub->dstprojplane);
-		draw_wall(pix, cub, ray, get_wall_text(cub, angle, ray.hor));
+		draw_stripe(pix, cub, ray, get_wall_text(cub, angle, ray.hor));
 		pix++;
 	}
 }
